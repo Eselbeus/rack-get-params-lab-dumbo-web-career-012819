4594228e03d40cf70a821b7809bd075861c3acb2
Seann Branchfield <eselbeus@gmail.com>
2019-02-17 01:56:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -21,7 +21,7 @@ class Application
     elsif req.path.match(/add/)
       add_item = req.params["item"]
       
-      if 
+      if @@items.include?()
     else
       resp.write "Path Not Found"
     end
